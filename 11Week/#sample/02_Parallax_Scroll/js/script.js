315de23327c6d1abaed7dfcdb3b466c5d189ab9a
rh <>
2018-11-08 02:05:06 +0900

    11주차 수업 준비.

---
+++ 11Week/#sample/02_Parallax_Scroll/js/script.js
@@ -0,0 +1,22 @@
+//jQuery Call.
+(function($){
+//Document Ready.
+$(document).ready(function(){
+    var Parallax = {
+        init : function() {
+            this.layout();
+            this.reset();
+            this.addEvent();
+        },
+        layout : function() {
+        },
+        reset : function() {
+        },
+        addEvent : function() {
+        },
+        resetInit : function() {
+        }
+    }
+    Parallax.init();
+});
+})(jQuery);
